66e3db3f3379b49406a6b7e6ca80c218e1697511
Yujie_Liu <136684976+RCrobotcat@users.noreply.github.com>
2025-11-09 00:11:47 +0800

    Added different kinds of lights

--- shaders/fragmentShader.fs
+++ shaders/fragmentShader.fs
@@ -2,18 +2,24 @@
 struct Material {
     sampler2D diffuse;
     sampler2D specular;
-    sampler2D emission;
     float shininess;
 };
 
 uniform Material material;
 
 struct Light {
-    vec3 position;
+    vec3  position;
+    vec3  direction;
+    float cutOff; // spotlight inner cutoff
+    float outerCutOff; // spotlight outer cutoff
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 uniform Light light;
@@ -30,12 +36,13 @@ out vec4 FragColor;
 
 void main()
 {
+    vec3 lightDir = normalize(light.position - FragPos);
+
     // ambient
     vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
     // diffuse
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
@@ -46,10 +53,20 @@ void main()
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
-    // emission
-    float emissionStrength = 3.0f;
-    vec3 emission = vec3(texture(material.emission, TexCoords)) * emissionStrength;
+    // Attenuation calculation
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    ambient  *= attenuation;
+    diffuse  *= attenuation;
+    specular *= attenuation;
 
-    vec3 result = (ambient + diffuse + specular + emission) * objectColor;
+    // spotlight (soft edges)
+    float theta     = dot(lightDir, normalize(-light.direction));
+    float epsilon   = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0); // 用于平滑边缘
+    diffuse  *= intensity;
+    specular *= intensity;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
 }
